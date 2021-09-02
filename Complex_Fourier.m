2330c866e7ef279b2bd00088941353c05ceb69b3
MHMD <56149395+mhmd-j@users.noreply.github.com>
2021-09-02 16:50:19 +0430

    Add files via upload

---
+++ Complex_Fourier.m
@@ -0,0 +1,102 @@
+clc
+clear
+close all
+% Im = imread('plot 3.png');
+% Cordmat = pic2points(Im);
+% Cordmat(:,2) = Cordmat(:,2)-mean(Cordmat (:,2));
+% Cordmat(:,1) = Cordmat(:,1)-mean(Cordmat (:,1));
+%DO NOT CHANGE THE ENDTIME, since we assumed that the period of the figure
+%is 1s
+endtime = 1;
+numpoints = 500;
+% define function
+t = linspace(0,endtime,numpoints);
+Cordmat(:,2) = (sin(2*t));
+Cordmat(:,1) = exp(cos(5*t));
+% transfer the center of the figure to the origin
+Cordmat(:,2) = Cordmat(:,2)-mean(Cordmat (:,2));
+Cordmat(:,1) = Cordmat(:,1)-mean(Cordmat (:,1));
+
+plot(Cordmat (:,1), Cordmat (:,2));
+grid on
+axis equal
+imgCord = Cordmat(:,2)*1i;
+realCord = Cordmat(:,1);
+Cord = realCord + imgCord;
+deltaT = t(2)-t(1);
+
+% K is number Fourier Coefficient 
+%(fourier coeffs actually are 2K+1 because each periodic vector has a conjogate)
+K = 50;
+n = -K:1:K;
+C =(exp(-n'*2*pi*1i*t)*Cord.*deltaT)/endtime;
+vecorg = exp(n'*2*pi*1i*t);
+F = C.'*vecorg;
+figure
+plot(real(F),imag(F))
+figAx = gcf;
+Ax = figAx.CurrentAxes;
+axis equal
+grid on
+xylim = [Ax.XLim, Ax.YLim];
+%%
+% h = animatedline;
+% axis([-0.8,0.4,-0.4,0.2])
+% x = real(F);
+% y = imag(F);
+% for k = 1:5:numpoints-4
+%     xvec = x(k:k+4);
+%     yvec = y(k:k+4);
+%     addpoints(h,xvec,yvec)
+%     drawnow
+% end
+%%
+ntF = C.*vecorg;
+xnOriginP = 0;
+ynOriginP = 0;
+xnOriginN = 0;
+ynOriginN = 0;
+X = 0; Y = 0;
+fig = figure('Name','Vectors');
+figure(fig)
+% plot(real(F),imag(F))
+% hold on
+axis equal
+for T = 1:length(ntF)
+    % T = 1
+    figure(fig)
+    xnOriginP = 0;
+    ynOriginP = 0;
+    xnOriginN = 0;
+    ynOriginN = 0;
+    for m = K+1:length(C)-1
+        % for positive coeficients
+        x = [xnOriginN, xnOriginN+real(ntF(m,T))];
+        y = [ynOriginN, ynOriginN+imag(ntF(m,T))];
+        xnOriginP = x(2);
+        ynOriginP = y(2);
+        plot(x,y,'LineWidth',1.5)
+        hold on
+        % for negative coeficients
+        x = [xnOriginP, xnOriginP+real(ntF(length(C)-m,T))];
+        y = [ynOriginP, ynOriginP+imag(ntF(length(C)-m,T))];
+        xnOriginN = x(2);
+        ynOriginN = y(2);
+        plot(x,y,'LineWidth',1.5)
+        hold on
+    end
+    x = [xnOriginN, xnOriginN+real(ntF(m+1,T))];
+    y = [ynOriginN, ynOriginN+imag(ntF(m+1,T))];
+    plot(x,y,'LineWidth',1.5)
+    hold on
+    X(T) = x(2);
+    Y(T) = y(2);
+    plot(X,Y)
+    hold on
+    plot(x(2),y(2),'*')
+    grid on
+%     axis(xylim)
+    axis equal
+    set(gcf,'Position',[200 50 1000 600])
+    hold off
+end
